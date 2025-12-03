81dff9d9720f3ba955a5e1c9a8f002543953c1d5
Christopher Phillips <shrydar@jaruth.com>
2025-12-03 16:18:05 +0800

    bump version to recognise v2 delay table inclusion.

--- src/mwax_udp2sub.c
+++ src/mwax_udp2sub.c
@@ -6,9 +6,10 @@
 //            CJP Christopher Phillips christopher.j.phillips@curtin.edu.au
 // Commenced 2017-05-25
 //
-#define BUILD 97
-#define THISVER "2.19"
+#define BUILD 98
+#define THISVER "2.20"
 //
+// 2.20-098     2025-11-26 CJP  New delay table format
 // 2.19-097     2025-02-24 CJP  quieter logging
 // 2.18-096     2025-02-24 CJP  free file selection bugfix
 // 2.17-095     2025-02-24 CJP  broaden free file selection criteria
